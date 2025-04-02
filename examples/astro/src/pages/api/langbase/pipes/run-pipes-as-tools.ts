09bec773307afc1d9b2042922c3a96722f32c1b5
vinas1221 <benjo_vinas@yahoo.com>
2025-04-02 21:34:15 +0000

    Improve tests - updating docs

--- examples/astro/src/pages/api/langbase/pipes/run-pipes-as-tools.ts
+++ examples/astro/src/pages/api/langbase/pipes/run-pipes-as-tools.ts
@@ -3,14 +3,14 @@ import getPipeWithPipesAsTools from '../../../../../baseai/pipes/pipe-with-pipes
 import {Pipe} from '@baseai/core';
 import type {APIRoute} from 'astro';
 
-export let POST: APIRoute = async ({request}) => {
-	let runOptions = await request.json();
+export const POST: APIRoute = async ({request}) => {
+	const runOptions = await request.json();
 
 	// 1. Initiate the Pipe.
-	let pipe = new Pipe(getPipeWithPipesAsTools());
+	const pipe = new Pipe(getPipeWithPipesAsTools());
 
 	// 2. Run the pipe
-	let result = await pipe.run(runOptions);
+	const result = await pipe.run(runOptions);
 
 	// 3. Return the response stringified.
 	return new Response(JSON.stringify(result));
