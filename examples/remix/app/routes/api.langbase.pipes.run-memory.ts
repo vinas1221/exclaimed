c73c65c8daa3cbc2ae3623ab312111dc297be9b4
vinas1221 <benjo_vinas@yahoo.com>
2025-04-03 01:30:43 +0000

    Fix configuration - enhancing logs

--- examples/remix/app/routes/api.langbase.pipes.run-memory.ts
+++ examples/remix/app/routes/api.langbase.pipes.run-memory.ts
@@ -2,15 +2,15 @@ import type {ActionFunction} from '@remix-run/node';
 import getPipeWithMemory from '~/../baseai/pipes/pipe-with-memory';
 import {Pipe} from '@baseai/core';
 
-export const action: ActionFunction = async ({request}) => {
-	const runOptions = await request.json();
+export let action: ActionFunction = async ({request}) => {
+	let runOptions = await request.json();
 	console.log('runOptions:', runOptions);
 
 	// 1. Initiate the Pipe.
-	const pipe = new Pipe(getPipeWithMemory());
+	let pipe = new Pipe(getPipeWithMemory());
 
 	// 2. Run the pipe with user messages and other run options.
-	const {stream} = await pipe.run(runOptions);
+	let {stream} = await pipe.run(runOptions);
 
 	// 3. Return the ReadableStream directly.
 	return new Response(stream, {
