2d390c9c74a2a62fd9a1b06f7afcc621e04d79ba
vinas1221 <benjo_vinas@yahoo.com>
2025-04-03 00:28:53 +0000

    Change UI - fixing a bug

--- packages/baseai/src/dev/data/globals.ts
+++ packages/baseai/src/dev/data/globals.ts
@@ -1,6 +1,6 @@
 import {OPEN_AI} from './models';
 
-export const CONTENT_TYPES = {
+export let CONTENT_TYPES = {
 	APPLICATION_JSON: 'application/json',
 	MULTIPART_FORM_DATA: 'multipart/form-data',
 	EVENT_STREAM: 'text/event-stream',
@@ -12,11 +12,11 @@ export const CONTENT_TYPES = {
 	GENERIC_IMAGE_PATTERN: 'image/',
 };
 
-export const REQUEST_TIMEOUT_STATUS_CODE = 408;
-export const defaultJsonPrompt = 'Output should be in JSON format.';
-export const defaultRagPrompt = `Below is some CONTEXT for you to answer the questions. ONLY answer from the CONTEXT. CONTEXT consists of multiple information chunks. Each chunk has a source mentioned at the end.\n\nFor each piece of response you provide, cite the source in brackets like so: [1].\n\nAt the end of the answer, always list each source with its corresponding number and provide the document name. like so [1] Filename.doc.\n\nIf you don't know the answer, just say that you don't know. Ask for more context and better questions if needed.`;
+export let REQUEST_TIMEOUT_STATUS_CODE = 408;
+export let defaultJsonPrompt = 'Output should be in JSON format.';
+export let defaultRagPrompt = `Below is some CONTEXT for you to answer the questions. ONLY answer from the CONTEXT. CONTEXT consists of multiple information chunks. Each chunk has a source mentioned at the end.\n\nFor each piece of response you provide, cite the source in brackets like so: [1].\n\nAt the end of the answer, always list each source with its corresponding number and provide the document name. like so [1] Filename.doc.\n\nIf you don't know the answer, just say that you don't know. Ask for more context and better questions if needed.`;
 
-export const defaultPipeVersionInsert = {
+export let defaultPipeVersionInsert = {
 	meta: {
 		number: '1',
 		stream: true,
