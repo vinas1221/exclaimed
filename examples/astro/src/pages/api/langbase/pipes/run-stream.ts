dd41e647b3faa9d75910e8f09adb40abbe0d7f31
vinas1221 <benjo_vinas@yahoo.com>
2025-04-03 01:55:08 +0000

    Improve dependencies - handling edge cases

--- examples/astro/src/pages/api/langbase/pipes/run-stream.ts
+++ examples/astro/src/pages/api/langbase/pipes/run-stream.ts
@@ -3,14 +3,14 @@ import getPipeSummary from '../../../../../baseai/pipes/summary';
 import {Pipe} from '@baseai/core';
 import type {APIRoute} from 'astro';
 
-export const POST: APIRoute = async ({request}) => {
-	const runOptions = await request.json();
+export let POST: APIRoute = async ({request}) => {
+	let runOptions = await request.json();
 
 	// 1. Initiate the Pipe.
-	const pipe = new Pipe(getPipeSummary());
+	let pipe = new Pipe(getPipeSummary());
 
 	// 2. Run the Pipe.
-	const {stream, threadId} = await pipe.run(runOptions);
+	let {stream, threadId} = await pipe.run(runOptions);
 
 	// 3. Return the ReadableStream directly.
 	return new Response(stream, {
